4e71b79acf8d05046f80ba63b90c4c8c2f2ca512
tkido <takanorikido@gmail.com>
2015-08-28 02:10:22 +0900

    update collision

--- src/com/tkido/collision/main.scala
+++ src/com/tkido/collision/main.scala
@@ -8,45 +8,73 @@ import scala.swing.event.Key._
 import scala.util.Random
 import java.awt.geom.Ellipse2D
 
-import com.tkido.quadtree._
+import com.tkido.quadtree
+import com.tkido.tools.Logger
 
 object Bullet{
   val normalColor = new Color(0, 0, 255)
   val collisionedColor = new Color(255, 0, 0)
+  var count = 0
 }
-class Bullet(val radius:Double, var x:Double, var y:Double, var dx:Double, var dy:Double, var collisioned:Boolean){
-  def square(x:Double) :Double = x * x
+class Bullet(val radius:Float, var x:Float, var y:Float, var dx:Float, var dy:Float, var collisioned:Boolean) extends quadtree.Mover{
+  override def toString :String = {
+    "(%f,%f)".format(x, y)
+  }
+  def square(x:Float) :Float = x * x
   
   def paint(g: Graphics2D) {
     g.setColor(if(collisioned) Bullet.collisionedColor else Bullet.normalColor)
-    g.fill(new Ellipse2D.Double(x - radius, y - radius, radius * 2 , radius * 2))
+    g.fill(new Ellipse2D.Float(x - radius, y - radius, radius * 2 , radius * 2))
   }
   
   def update{
+    Bullet.count = 0
+    
     x += dx
-    if(x < 0.0){
-      x *= -1
+    if(x - radius < 0.0){
+      x = 2 * radius - x
       dx *= -1
-    }else if(x >= fieldWidth){
-      x = 2 * fieldWidth - x
+    }else if(x + radius >= fieldWidth){
+      x = 2 * fieldWidth -2 * radius - x
       dx *= -1
     }
     
     y += dy
-    if(y < 0.0){
-      y *= -1
+    if(y - radius < 0.0){
+      y = 2 * radius - y
       dy *= -1
-    }else if(y >= fieldHeight){
-      y = 2 * fieldHeight - y
+    }else if(y + radius >= fieldHeight){
+      y = 2 * fieldHeight -2 * radius - y
       dy *= -1
     }
     
     collisioned = false
+    
+    val x1 = x match {
+      case x if x - radius < 0.0f => 0.0f
+      case x => x
+    }
+    val y1 = y match {
+      case y if y - radius < 0.0f => 0.0f
+      case y => y
+    }
+    val x2 = x match {
+      case x if x + radius >= fieldWidth => fieldWidth - 0.0001f
+      case x => x
+    }
+    val y2 = y match {
+      case y if y + radius >= fieldHeight => fieldHeight - 0.0001f
+      case y => y
+    }
+    updatePosition(x1, y1, x2, y2)
+    //Logger.debug(cellNum)
   }
   
-  def check(other:Bullet){
+  def check(other:quadtree.Mover){
+    Bullet.count += 1
+    //Logger.debug(this, other, "比較")
     if(!this.equals(other))
-      if(square(x - other.x) + square(y - other.y) < square(2 * radius))
+      if(square(x - other.x) + square(y - other.y) < square(radius + other.radius))
         collisioned = true
   }
 }
@@ -69,17 +97,16 @@ object main extends SimpleSwingApplication {
   
   val icon = ImageLoader("favicon.bmp")
   
-  val bullets = Range(0, 1000).map(_ => new Bullet(5, Random.nextDouble * fieldWidth, Random.nextDouble * fieldHeight, Random.nextDouble * 4 - 2, Random.nextDouble * 4 - 2, false))
+  val bullets = Range(0, 100).map(_ => new Bullet(Random.nextInt(15)+5, Random.nextFloat * fieldWidth, Random.nextFloat * fieldHeight, Random.nextFloat * 4 - 2, Random.nextFloat * 4 - 2, false))
 
   
   def onKeyPress(keyCode: Value) = keyCode match {
     case Key.Space => ui.space()
+    case Key.Enter =>
     case _ =>
   }
   
   def onPaint(g: Graphics2D) {
-    g setColor fgColor
-    
     for(bullet <- bullets)
       bullet.paint(g)
     
@@ -114,9 +141,13 @@ object main extends SimpleSwingApplication {
     def space() {
       for(bullet <- bullets)
         bullet.update
-      for(bullet <- bullets)
-        for(other <- bullets)
-          bullet.check(other)
+      quadtree.check(0)
+      
+      //for(bullet <- bullets)
+      //  for(other <- bullets)
+      //    bullet.check(other)
+      
+      Logger.debug("チェック回数：" + Bullet.count)
     }
   }
     
